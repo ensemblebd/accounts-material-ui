97091c56a9677259d5b19f3c9885994abbeb3798
barry durand <barrydurand87@gmail.com>
2020-10-26 22:39:00 -0500

    updated to latest meteor, w/o integrity check

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.1.1');
+  api.versionsFrom('1.11.1');
   api.use('ecmascript');
 	api.use('std:accounts-ui');
 	api.use('tmeasday:check-npm-versions@0.3.1');
